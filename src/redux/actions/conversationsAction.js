847f143f3504322a865b84359e144d54db572a03
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-10-28 10:46:31 -0700

    Update conversationsAction.js

--- src/redux/actions/conversationsAction.js
+++ src/redux/actions/conversationsAction.js
@@ -15,7 +15,7 @@ export const getConversations = () => (dispatch, getState) => {
   }
 
   axios
-    .get("https://messaging-web-app-server.donovanrowzee.net/conversations", config)
+    .get("https://messaging-web-app-server.herokuapp.com/conversations", config)
     .then((res) => {
       dispatch({
         type: GET_CONVERSATIONS,
